d598c73c98c8223c7428f4511b6508bb2edab2ab
Kovalev Vasilii <32467326+KovalevVasilii@users.noreply.github.com>
2018-02-04 00:55:48 +0300

    First Step

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+
+    public float speed;
+    public Transform Player;
+  
+    void FixedUpdate()
+    {   Rigidbody2D r2d = GetComponent<Rigidbody2D>();
+
+        float z = Mathf.Atan2((Player.transform.position.y - transform.position.y),
+            (Player.transform.position.x - transform.position.x)) *Mathf.Rad2Deg-90;
+            transform.eulerAngles = new Vector3(0, 0, z);
+        r2d.AddForce(gameObject.transform.up * speed);
+    }
+}
---
+++ Assets/Scripts/PlayerMobility.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMobility : MonoBehaviour
+{
+    public float speed;
+    Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("Attack");
+        }
+    }
+    void FixedUpdate()
+    {
+        Rigidbody2D r2d= GetComponent<Rigidbody2D>();
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+         Debug.Log(mousePosition);
+         Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
+        transform.rotation = rot;
+        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+
+        float input = Input.GetAxis("Vertical");
+        r2d.AddForce(gameObject.transform.up * speed * input);
+    }
+
+}
+
+
+
