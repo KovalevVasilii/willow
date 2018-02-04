ae4ae7c47e4af5f1406d6114a516803cb987f1c2
Kovalev Vasilii <32467326+KovalevVasilii@users.noreply.github.com>
2018-02-05 00:00:22 +0300

    Some fix

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -9,10 +9,27 @@ public class EnemyScript : MonoBehaviour {
   
     void FixedUpdate()
     {   Rigidbody2D r2d = GetComponent<Rigidbody2D>();
-
-        float z = Mathf.Atan2((Player.transform.position.y - transform.position.y),
-            (Player.transform.position.x - transform.position.x)) *Mathf.Rad2Deg-90;
+        float z;
+        if (Player!=null)
+        {
+             z= Mathf.Atan2((Player.transform.position.y - transform.position.y),
+            (Player.transform.position.x - transform.position.x)) * Mathf.Rad2Deg - 90;
             transform.eulerAngles = new Vector3(0, 0, z);
+        }
+        else
+        {
+            z = Random.value;
+            transform.eulerAngles = new Vector3(0, 0, z);
+        }
+       
         r2d.AddForce(gameObject.transform.up * speed);
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag=="Playr")
+        {
+            Destroy(other.gameObject);
+        }
+       
     }
 }
--- Assets/Scripts/PlayerMobility.cs
+++ Assets/Scripts/PlayerMobility.cs
@@ -1,36 +1,54 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
 
 public class PlayerMobility : MonoBehaviour
 {
-    public float speed;
-    Animator anim;
+
+    public float speed = 1.5f;
+    public float acceleration = 100;
+ 
+    private Vector3 direction;
+    private Rigidbody2D body;
+
     void Start()
     {
-        anim = GetComponent<Animator>();
+        body = GetComponent<Rigidbody2D>();
+        body.freezeRotation = true;
+        body.gravityScale = 0;
     }
-    void Update()
-    {
-        if(Input.GetMouseButtonDown(0))
-        {
-            anim.SetTrigger("Attack");
-        }
-    }
+
     void FixedUpdate()
     {
-        Rigidbody2D r2d= GetComponent<Rigidbody2D>();
-        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-         Debug.Log(mousePosition);
-         Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
-        transform.rotation = rot;
-        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+        body.AddForce(direction * body.mass * speed * acceleration);
+        gameObject.tag = "Player";
+        if (Mathf.Abs(body.velocity.x) > speed)
+        {
+            body.velocity = new Vector2(Mathf.Sign(body.velocity.x) * speed, body.velocity.y);
+        }
 
-        float input = Input.GetAxis("Vertical");
-        r2d.AddForce(gameObject.transform.up * speed * input);
+        if (Mathf.Abs(body.velocity.y) > speed)
+        {
+            body.velocity = new Vector2(body.velocity.x, Mathf.Sign(body.velocity.y) * speed);
+        }
+
     }
 
+    void LookAtCursor()
+    {
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // Debug.Log(mousePosition);
+        Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
+        transform.rotation = rot;
+        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+    }
+
+    void Update()
+    {
+        direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        LookAtCursor();
+    }
+   
 }
-
-
-
